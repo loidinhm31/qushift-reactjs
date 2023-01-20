d35bd910cbafd12a90958ef0cb710bb0d1429b1a
loidinhm31 <loidinh.git@gmail.com>
2023-01-20 13:52:53 +0700

    Reformat: format code

--- src/app/core/components/Messages/InputBox/CommunicateInterface.tsx
+++ src/app/core/components/Messages/InputBox/CommunicateInterface.tsx
@@ -3,13 +3,11 @@ export interface TopicInterface {
     name: string;
     members?: MemberInterface[];
 }
-
 export interface MemberInterface {
     user: string;
     checkSeen: boolean;
     notSeenCount: number;
 }
-
 export interface CommunicatePropInterface {
     userId: string;
     topicId: string;
