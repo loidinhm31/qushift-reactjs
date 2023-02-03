ab2614cb4dc9d03cb201724cb3bab9bbb66d3c80
loidinhm31 <loidinh.git@gmail.com>
2023-02-04 02:41:11 +0700

    API: change PUT method for send signal

--- ts-qushift-fe/src/pages/api/messages/send_signal.ts
+++ ts-qushift-fe/src/pages/api/messages/send_signal.ts
@@ -1,11 +1,14 @@
+import { getSession } from "next-auth/react";
+
 const handler = async (req, res) => {
+	const session = await getSession({ req });
 	const { currTopicId } = req.body;
 
 	const requestOptions = {
-		method: "POST",
-	}
+		method: "PUT"
+	};
 
-	const response = await fetch(`${process.env.API_BASE_URL}/topics/signal/${currTopicId}`, requestOptions);
+	const response = await fetch(`${process.env.API_BASE_URL}/topics/signal/${currTopicId}?userId=${session.user.id}`, requestOptions);
 
 	res.status(200).json({});
 };
